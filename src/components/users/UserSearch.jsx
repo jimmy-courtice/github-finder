accd3dbb6f10fead4bbc59e2babdd8e1ed03081c
Jimmy Courtice <jimmy.courtice@vmlyr.com>
2022-07-02 00:16:59 -0500

    Finishing up finder app

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,20 +1,24 @@
 import {useState,useContext} from 'react'
 import GithubContext from '../../context/github/GithubContext'
+import AlertContext from '../../context/alert/AlertContext'
+import {searchUsers} from '../../context/github/GithugActions'
 
 function UserSearch() {
     const [text, setText] = useState('')
 
-    const {users, searchUsers} = useContext(GithubContext)
+    const {users, dispatch} = useContext(GithubContext)
+    const {setAlert} = useContext(AlertContext)
 
     const handleChange = (e) => setText(e.target.value)
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         if (text === '') {
-            alert('Please enter something.')
+            setAlert('Please enter something.', 'error')
         } else {
-            // Search users
-            searchUsers(text)
+            dispatch({type: 'SET_LOADING'})
+            const users = await searchUsers(text)
+            dispatch({type: 'GET_USERS', payload: users})
             setText('')
         }
     }
@@ -41,7 +45,7 @@ function UserSearch() {
         </div>
         {users.length > 0 && (
            <div>
-            <button type='submit' className="btn btn-ghost btn-lg">
+            <button onClick={() => dispatch({ type: 'CLEAR_USERS'})} type='submit' className="btn btn-ghost btn-lg">
                 Clear
             </button>
         </div> 
